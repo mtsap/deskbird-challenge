afe7c86b5d40c2020c7739ef32c6620b46202e4a
Michael Tsapanos <mich.tsap@gmail.com>
2025-07-18 15:34:09 +0300

    feat: add logging middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { LoggingMiddleware } from './logging/middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
